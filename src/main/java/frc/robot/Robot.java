9f4e21f848e4a14a083a605d9229344747cc5863
Ryan10145 <chang.ryan10145@gmail.com>
2020-01-07 21:26:28 -0500

    Add Basic Project

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+
+    private RobotContainer robotContainer;
+
+    @Override
+    public void robotInit() {
+        robotContainer = new RobotContainer();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+}
