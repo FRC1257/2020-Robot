82b443e9f7911ac6bb2fae9c619e0200d3407426
EthanMuchnik <emscientist@gmail.com>
2020-01-18 16:49:42 -0500

    Finished simple version of code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@ import frc.robot.commands.intake.*;
 import frc.robot.commands.indexer.*;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -15,27 +17,32 @@ import frc.robot.subsystems.*;
 
 public class RobotContainer {
 
-    private final XboxController driveController;
     private final XboxController operatorController;
     private final Intake intake;
     private final Indexer indexer;
 
 
     public RobotContainer() {
-        driveController = new XboxController(CONTROLLER_DRIVE_ID);
         operatorController = new XboxController(CONTROLLER_OPERATOR_ID);
 
         intake = new Intake();
         intake.setDefaultCommand(new IntakeNeutralCommand(intake));
         
         indexer = new Indexer();
-        indexer.setDefaultCommand(new IntakeNeutralCommand(intake));
+        indexer.setDefaultCommand(new IndexerNeutralCommand(indexer));
 
         configureButtonBindings();
     }
 
     // defines button -> command mappings
     private void configureButtonBindings() {
-        
+        //Intake Bindings
+        (new JoystickButton(operatorController, Button.kA.value)).whenPressed(new IntakeEjectCommand(intake));
+        (new JoystickButton(operatorController, Button.kB.value)).whenPressed(new IntakeIntakeCommand(intake));
+
+        (new JoystickButton(operatorController, Button.kX.value)).whenPressed(new IndexerIntakeCommand(indexer));
+        (new JoystickButton(operatorController, Button.kY.value)).whenPressed(new IndexerIntakeCommand(indexer));
+        (new JoystickButton(operatorController, Button.kBumperLeft.value)).whenPressed(new IndexerIntakeCommand(indexer));
+        (new JoystickButton(operatorController, Button.kBumperRight.value)).whenPressed(new IndexerIntakeCommand(indexer));
     }
 }
