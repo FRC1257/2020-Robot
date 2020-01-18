7290082c3e9b7d74c848f4c60a4c797d4acf67de
EthanMuchnik <emscientist@gmail.com>
2020-01-18 12:57:02 -0500

    More Intake Command Changes

---
+++ src/main/java/frc/robot/commands/Intake/IntakeNeutralCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class IntakeNeutralCommand extends CommandBase {
+
+    private final Intake intake;
+    public IntakeNeutralCommand(Intake intake) {
+        this.intake = intake;
+        
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        intake.neutral();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
